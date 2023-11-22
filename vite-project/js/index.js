53b6722fe57e912fa901d6025e7280d56f066a07
evY000 <000evan001@gmail.com>
2023-11-21 23:50:11 -0500

    qwsdaefrgt,l./

---
+++ vite-project/js/index.js
@@ -0,0 +1,15 @@
+
+document.querySelector("button").addEventListener("click", function(){
+   if (document.body.classList.contains("dark")){
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");}
+      else{
+         document.body.classList.add("dark");
+         document.body.classList.remove("light");}
+
+      });
+//const card = document.getElementById("card").innerHTML=card;
+//const filterplayers = card.filter((card)=> card.length > 4);
+//function greaterthanfour(){
+ //   filterplayers.foreach(card => card.length < 4 === card.style.display === "none");
+//}
