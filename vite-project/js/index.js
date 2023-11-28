d1229802689445cd6cd78bde35da4c6e1c7a0a8b
evY000 <000evan001@gmail.com>
2023-11-28 13:13:27 -0500

    -asqthyjkl;'

--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -1,9 +1,16 @@
+import '../styles/style.css ';
 
-document.querySelector("button").addEventListener("click", function(){
+const DOMSelectors = {
+   color: document.querySelector('#color')
+}
+
+
+
+DOMSelectors.color.addEventListener("click", function(){
    if (document.body.classList.contains("dark")){
       document.body.classList.add("light");
-      document.body.classList.remove("dark");}
-      else{
+      document.body.classList.remove("dark");
+   } else{
          document.body.classList.add("dark");
          document.body.classList.remove("light");}
 
