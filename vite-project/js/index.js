68ae4c83d0946dd18f33bab287ded6343c617cee
evY000 <000evan001@gmail.com>
2023-12-05 13:15:15 -0500

    qaswghyjuki

--- vite-project/js/index.js
+++ vite-project/js/index.js
@@ -2,9 +2,11 @@ import '../styles/style.css';
 import {cards} from './main.js';
 const DOMSelectors = {
    color: document.querySelector('#color'),
-   Thing: document.getElementById("Card")
+   Thing: document.getElementById("Card"),
+   btn : document.getElementById("btn"),
+   app : document.getElementById('app')
 }
- // function populate(arr){
+
 cards.forEach((card)=> {
    const FirstName = card.FirstName
    const LastName = card.LastName
@@ -15,30 +17,18 @@ const card2 = ` <div class = "Card">
 <img class ="imgs" src="${Image}"</div>`
 DOMSelectors.Thing.insertAdjacentHTML('afterend', card2);
 })
-DOMSelectors.btn.addEventListener("click", GOATFilter());
-function GOATFilter(){
-   const GOATS = cards.filter(card => card.GOAT.includes("Yes"));
-   DOMSelectors.Thing.innerHTML = "";
-   GOATS.forEach((card) =>{
-      const FirstName = card.FirstName
-   const LastName = card.LastName
-   const Image = card.IMG
-const card2 = ` <div class = "Card">
-<h1> ${FirstName} </h1>
-<h2> ${LastName} </h2> 
-<img class ="imgs" src="${Image}"</div>`
-DOMSelectors.Thing.insertAdjacentHTML('afterend', card2);  })
-}
-GOATFilter()
 
-/*  function filter(){
-   let button = document.querySelectorAll("#btn")
-   button.forEach((btn)=> btn.addEventListener("click", function(){
-      let GOATFilter = cards.filter((cards)=> cards.GOAT.includes("Yes"),
-      document.querySelector(".parent").innerHTML = "",
-      populate(GOATFilter),
- }))}
- */
+function CardFilter(){
+   DOMSelectors.btn.addEventListener("click",function(){
+      DOMSelectors.app.innerHTML = ''
+      const filterCards = cards.filter((card) => card.GOAT === "Yes");
+      const b = filterCards.FirstName
+      const a = `<div class = "Card"> <h1>${b}</h1> </div>`
+      DOMSelectors.app.insertAdjacentHTML('beforeend',  `${a}` );
+      });}
+CardFilter()
+
+
 
 DOMSelectors.color.addEventListener("click", function(){
    if (document.body.classList.contains("dark")){
